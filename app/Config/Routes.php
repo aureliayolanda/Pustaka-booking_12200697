36f6a3f97eebc14f61ca1cdc9cb4500cec2d51e6
aureliayolanda <aureliayolada97@gmail.com>
2021-09-25 12:36:57 +0700

    tugas pertemuan 2

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -32,7 +32,9 @@ $routes->setAutoRoute(true);
 // We get a performance increase by specifying the default
 // route since we don't have to scan directories.
 $routes->get('/', 'Home::index');
-
+$routes->get(from: '/hello', to: 'SelamatDatang::hal_awal');
+$routes->get(from: '/login', to: 'SelamatDatang::beranda_login');
+$routes->get(from: '/daftar-member', to: 'SelamatDatang::daftar_member');
 /*
  * --------------------------------------------------------------------
  * Additional Routing
