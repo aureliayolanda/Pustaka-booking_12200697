90528e5c5c8e4525f29189b144a6949d0bd9792a
aureliayolanda <aureliayolada97@gmail.com>
2021-10-01 19:56:53 +0700

    tugas pertemuan 2

--- app/Views/halaman/login.php
+++ app/Views/halaman/login.php
@@ -8,7 +8,7 @@
     <h1>Selamat datang di Sistem Informasi Perpustakaan</h1>
     <p>
         Ini adalah layanan perpustakaan digital masa kini
-        <a href="<?= base_url(relativePath: '/daftar-member') ?>">klik daftar</a> untuk mendaftar menjadi member
+        <a href="<?= base_url('/daftar-member') ?>">klik daftar</a> untuk mendaftar menjadi member
     </p>
 </body>
 
